11d433d7aa5830f75b002b7eb14e35bafba31eef
Destiny Sparta <spartanunlock2@gmail.com>
2025-09-09 22:44:37 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Tife Tech',
   botName: "QUEEN MARVEL V2",
   botOwner: 'Tife Tech', // Your name
-  ownerNumber: '2348136483096', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '233547998018', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "one of the best whatsapp bots in the world .",
